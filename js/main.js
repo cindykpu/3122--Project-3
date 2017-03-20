f57d8243e02e49a3b3ff827db6eea8abfe13101f
cindykpu <CindyKong@016-GDMA-Mac.kwantlen.ca>
2017-03-20 13:57:49 -0700

    new new

--- js/main.js
+++ js/main.js
@@ -1,1 +1,39 @@
+$(".switch").click (function(){
+  $(".shoe-box").fadeIn();
+  $(".color-shoe-box").fadeOut();
+  $(".shoe-two").fadeOut();
+  $(".shoe-three").fadeOut();
+  $(".shoe-four").fadeOut();
+});
 
+$(".switch-two").click (function(){
+  $(".shoe-box").fadeOut();
+  $(".color-shoe-box").fadeIn();
+  $(".shoe-two").fadeOut();
+  $(".shoe-three").fadeOut();
+  $(".shoe-four").fadeOut();
+});
+
+$(".switch-three").click (function(){
+  $(".shoe-box").fadeOut();
+  $(".color-shoe-box").fadeOut();
+  $(".shoe-two").fadeIn();
+  $(".shoe-three").fadeOut();
+  $(".shoe-four").fadeOut();
+});
+
+$(".switch-four").click (function(){
+  $(".shoe-box").fadeOut();
+  $(".color-shoe-box").fadeOut();
+  $(".shoe-two").fadeOut();
+  $(".shoe-three").fadeIn();
+  $(".shoe-four").fadeOut();
+});
+
+$(".switch-five").click (function(){
+  $(".shoe-box").fadeOut();
+  $(".color-shoe-box").fadeOut();
+  $(".shoe-two").fadeOut();
+  $(".shoe-three").fadeOut();
+  $(".shoe-four").fadeIn();
+});
